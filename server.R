a1c39fd4b43b14e6d1eaaf6c2b559c5a0984b5a5
joschafer <j_o_schafer@yahoo.com>
2015-01-15 13:28:56 -0800

    Changes to documentation and UI

--- server.R
+++ server.R
@@ -4,31 +4,42 @@
 library(shiny)
 library(zoo, warn.conflicts = FALSE)
 
+MAXROWS <- 10000
 mav <- function(x,n=5){filter(x,rep(1/n,n), sides=2)}
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
+    # Generate a time series plot and return plot to UI.
+    # Call is reactive so it updates on input change.
+    #
 
-  output$distPlot <- renderPlot({
-      # Read in file 
-      inFile <- input$file1
-
-      if (is.null(inFile))
-          return(NULL)
+    output$distPlot <- renderPlot({
+      
+        # Read in file
+        # File should be 1 column of TS data values or 2 columns with the sequence 
+        #   in column 1 and the TS data value in column 2.  Data is limited to 10000
+        #   rows.
+        inFile <- input$file1
+        if (is.null(inFile))
+            return(NULL)
+        seriesData <- read.csv(inFile$datapath, nrows=MAXROWS)
+        
+        # Process 1 col and 2 col files appropriately
+        if (ncol(seriesData) == 1) {
+            # Calculate moving average
+            moving <- mav(seriesData, n=input$samples)
+            # draw the time series plot with the moving average overlay
+            myPlot <- ts.plot(seriesData, moving, gpars = list(col = c("black", "red")))
+        } else if (ncol(seriesData) == 2) {
+            # Calculate moving average
+            moving <- mav(seriesData[2], n=input$samples)
+            # draw the time series plot with the moving average overlay
+            myPlot <- ts.plot(seriesData[2], moving, gpars = list(col = c("black", "red")))
+        } else {
+            return(NULL)
+        }
     
-    seriesData <- read.csv(inFile$datapath)
-    moving <- mav(seriesData, n=input$samples)
-    
-    # draw the time series plot with the moving average overlay
-    myPlot <- ts.plot(seriesData, moving, gpars = list(col = c("black", "red")))
-    
-  })
+    })
 })
 
--- ui.R
+++ ui.R
@@ -7,17 +7,19 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Moving Average"),
-
+  ## TODO Add documentation
+  p("The Moving Average application takes a file with time series data inside and provides both a simple display of the data and a display of a moving average for the data. The file can be a 1 column or 2 column CSV file with the time series data in the last column.  It is ecpected to be in order.  The other limitaion is that the data load is limited to 10,000 samples.  In addition to displaying the data, the moving everage window can be adjusted from 2 to 40 bins with a default size of 5 bins."),
+  
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(fileInput('file1', 'Choose CSV File',
                 accept=c('text/csv',
                     'text/comma-separated-values,text/plain', '.csv')),
       sliderInput("samples",
-                  "Samples in MVA:",
+                  "Samples in Moving Average:",
                   min = 2,
                   max = 40,
-                  value = 10)
+                  value = 5)
     ),
 
     # Show a plot of the generated distribution
